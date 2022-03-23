dbac6515d08ef32d2f9629fde9249b15b1bd84bc
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2022-03-23 18:31:49 -0400

    adding skeleton files

---
+++ ui/components/app/token-detected/token-detected-details/token-detected-details.js
@@ -0,0 +1,18 @@
+import React, { useContext } from 'react';
+import { I18nContext } from '../../../../contexts/i18n';
+
+import Box from '../../../ui/box/box';
+
+const TokenDetectedDetails = () => {
+    const t = useContext(I18nContext);
+
+    return (
+        <Box></Box>
+    );
+};
+
+// TokenDetectedDetails.propTypes = {
+//     address: PropTypes.string,
+//   };
+  
+  export default TokenDetectedDetails;
